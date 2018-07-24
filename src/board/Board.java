f2f50af9269bfe2411d61887d3150b240f0b3736
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-07-24 13:42:10 -0400

    added rows and columns to Board class

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,19 +2,38 @@ package board;
 
 public class Board {
 
-	public static final int SIZE = 9;
-	private int[][] tiles;
+	private final int SIZE;
 
-	public Board(int[] startingState) {
-		/*
-		 * startingState is one dimensional array with all tile states, from left to
-		 * right, top to bottom. empty tiles are 0
-		 */
-		this.tiles = new int[SIZE][SIZE];
+	private int[][] rows;
+	private int[][] columns;
 
-		for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {// convert one dimensional input array to 2D tiles array
+	public final int getSize() {
+		return (SIZE);
+	}
+
+	public int[] getRow(int row) {//returns the specified row
+		return (rows[row]);
+	}
+
+	public int[] getColumn(int column) {//returns the specified column
+		return (columns[column]);
+	}
+
+	public Board(int size, int[] startingState) {
+		//startingState is one dimensional array with all tile states, from left to right, top to bottom. empty tiles are 0
+		this.SIZE = size;
+		this.rows = new int[SIZE][SIZE];
+		this.columns = new int[SIZE][SIZE];
+
+		for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {// convert one dimensional input array to 2D rows array
 			for (int j = 0; j < SIZE; j++) {
-				tiles[i / SIZE][j] = startingState[i + j];
+				rows[i / SIZE][j] = startingState[i + j];
+			}
+		}
+
+		for (int j = 0; j < SIZE; j++) {// convert one dimensional input array to 2D columns array
+			for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {
+				columns[j][i / SIZE] = startingState[i + j];
 			}
 		}
 
