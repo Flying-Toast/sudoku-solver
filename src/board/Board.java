1ed00f5d2961d74ae0b5dfd8c06fcdc4925ecdfa
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-07-24 21:03:01 -0400

    removed broken code

--- src/board/Board.java
+++ src/board/Board.java
@@ -10,50 +10,40 @@ public class Board {
 		return (SIZE);
 	}
 
-	private Tile[] getRow(int row) {//returns the specified row
+	private Tile[] getRow(int rowIndex) {//returns the specified row
 		Tile[][] rows = new Tile[SIZE][SIZE];
 		for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {// convert one dimensional state array to 2D rows array
 			for (int j = 0; j < SIZE; j++) {
 				rows[i / SIZE][j] = state[i + j];
 			}
 		}
-		return (rows[row]);
+		return (rows[rowIndex]);
 	}
 
-	private Tile[] getColumn(int column) {//returns the specified column
+	private Tile[] getColumn(int columnIndex) {//returns the specified column
 		Tile[][] columns = new Tile[SIZE][SIZE];
 		for (int j = 0; j < SIZE; j++) {// convert one dimensional state array to 2D columns array
 			for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {
 				columns[j][i / SIZE] = state[i + j];
 			}
 		}
-		return (columns[column]);
+		return (columns[columnIndex]);
 	}
 
-	public Tile[] getBox(int box) {//box is a 3x3 grid on the board, box index ("box" parameter) is from left to right, top to bottom
-		Tile[][] boxes = new Tile[SIZE][SIZE];
-		for (int verticalBox = 0; verticalBox < (int) Math.pow(SIZE, 2); verticalBox += SIZE * BOX_SIZE) {
-			for (int horizontalBox = 0; horizontalBox < SIZE; horizontalBox += BOX_SIZE) {
-				for (int column = 0; column < SIZE * BOX_SIZE; column += SIZE) {
-					for (int tileInRow = 0; tileInRow < BOX_SIZE; tileInRow++) {
-						boxes[(horizontalBox / BOX_SIZE) + (verticalBox / (SIZE * BOX_SIZE))][tileInRow + (BOX_SIZE * (column / SIZE))] = state[tileInRow + column + horizontalBox];
-					}
-				}
-			}
-		}
-		return (boxes[box]);
+	public Tile[] getBox(int boxIndex) {//box is a 3x3 grid on the board, box indices are from left to right, top to bottom, where each box grid has one index
+		//TODO: implement
 	}
 
 	//	public Tile[] getRowByTileBoardIndex() {
-	//		return();//TODO: implement
+	//		//TODO: implement
 	//	}
 	//
 	//	public Tile[] getColumnByTileBoardIndex() {
-	//		return();//TODO: implement
+	//		//TODO: implement
 	//	}
 	//
 	//	public Tile[] getBoxByTileBoardIndex() {
-	//		return();//TODO: implement
+	//		//TODO: implement
 	//	}
 
 	public Board(int size, int[] startingState) {
