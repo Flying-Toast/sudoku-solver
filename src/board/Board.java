110b470242cf789292c86df76669c274c0d057b3
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-07-25 16:27:51 -0400

    getBoxByTileBoardIndex working!!!!!!!!

--- src/board/Board.java
+++ src/board/Board.java
@@ -15,7 +15,7 @@ public class Board {
 	}
 
 	private Tile[] getRow(int rowIndex) {//returns the specified row (zero based rowIndex)
-		Tile[][] rows = new Tile[SIZE][SIZE];
+		Tile[][] rows = new Tile[SIZE][SIZE];//TODO: only build row that is needed, not entire board.
 		for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {// convert one dimensional state array to 2D rows array
 			for (int j = 0; j < SIZE; j++) {
 				rows[i / SIZE][j] = state[i + j];
@@ -25,7 +25,7 @@ public class Board {
 	}
 
 	private Tile[] getColumn(int columnIndex) {//returns the specified column (zero based columnIndex)
-		Tile[][] columns = new Tile[SIZE][SIZE];
+		Tile[][] columns = new Tile[SIZE][SIZE];//TODO: only build column that is needed, not entire board.
 		for (int j = 0; j < SIZE; j++) {// convert one dimensional state array to 2D columns array
 			for (int i = 0; i < (int) Math.pow(SIZE, 2); i += SIZE) {
 				columns[j][i / SIZE] = state[i + j];
@@ -33,10 +33,6 @@ public class Board {
 		}
 		return (columns[columnIndex]);
 	}
-
-	//		public Tile[] getBox(int boxIndex) {//box is a 3x3 grid on the board, box indices are from left to right, top to bottom, where each box grid has one index
-	//			//TODO: implement
-	//		}
 
 	public Tile[] getRowByTileBoardIndex(int tileIndex) {//returns the row that contains the tile of the given boardIndex (i.e TileInstance.getBoardIndex())
 		Tile[] row = new Tile[SIZE];
@@ -50,9 +46,24 @@ public class Board {
 		return (column);
 	}
 
-	//		public Tile[] getBoxByTileBoardIndex() {
-	//			//TODO: implement
-	//		}
+	public Tile[] getBoxByTileBoardIndex(int tileIndex) {//box is a 3x3 grid (on a standard 9x9) board, box indices are from left to right, top to bottom, where each box grid has one index
+		Tile[] box = new Tile[SIZE];
+		int indexOfLeftmostTileInBox = (tileIndex - (tileIndex % BOX_SIZE)); //indexOfLeftmostTileInBox: the boardIndex of the leftmost tile in the box that is in the same row as boxIndex
+		int rowOfTileIndex = (tileIndex - ((tileIndex % SIZE))) / SIZE;//the row index of the tile located at tileIndex
+		int indexOfFirstTileInBox = indexOfLeftmostTileInBox - (SIZE * (rowOfTileIndex % BOX_SIZE));//the boardIndex of the first tile in the box at boxIndex
+
+		int localTileIndex = 0;
+		for (int row = 0; row < BOX_SIZE; row++) {
+			for (int tileInRow = 0; tileInRow < BOX_SIZE; tileInRow++) {
+				int s = indexOfFirstTileInBox + tileInRow + (row * SIZE);
+				int b = localTileIndex;
+				box[b] = state[s];
+				localTileIndex++;//localTileIndex: zero based index of the tile in the box that is currently being inserted, with the zero index being the top left tile in the box
+			}
+		}
+
+		return (box);
+	}
 
 	public Board(int size, int[] startingState) {
 		//startingState is one dimensional array with all tile states, from left to right, top to bottom. empty tiles are 0
